60bc8dacebbf69103c1e15195e67636ec20c5fce
Fadwa Ali <75944731+Fadwahigga@users.noreply.github.com>
2024-06-22 23:59:13 +0300

    edit

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -2,16 +2,21 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\StateResource\Pages;
-use App\Filament\Resources\StateResource\RelationManagers;
-use App\Models\State;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\State;
+use App\Models\Country;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\StateResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\StateResource\RelationManagers;
 
 class StateResource extends Resource
 {
@@ -23,7 +28,13 @@ class StateResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Section::make()->schema([
+                    TextInput::make('name'),
+                    Select::make('country_id')
+                        ->label('Country')
+                        ->options(Country::all()->pluck('name', 'id'))
+                        ->searchable()
+                ]),
             ]);
     }
 
@@ -31,7 +42,11 @@ class StateResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('name')->sortable()->searchable(),
+                TextColumn::make('country.name')->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
             ])
             ->filters([
                 //
