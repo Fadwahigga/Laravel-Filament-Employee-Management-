1f0147d1294abf72ed4b6baf847a9f797d4e35f8
Fadwa Ali <75944731+Fadwahigga@users.noreply.github.com>
2024-06-23 01:31:25 +0300

    edit

--- app/Filament/Resources/CityResource.php
+++ app/Filament/Resources/CityResource.php
@@ -23,7 +23,7 @@ class CityResource extends Resource
 {
     protected static ?string $model = City::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-building-office-2';
     protected static ?string $navigationGroup = 'SYSTEM MANAGEMENT';
 
     public static function form(Form $form): Form
