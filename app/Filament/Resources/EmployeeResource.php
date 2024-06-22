41cd96d1fca45cacbab4779b285c096082dcd0fb
Fadwa Ali <75944731+Fadwahigga@users.noreply.github.com>
2024-06-23 00:22:54 +0300

    edit

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -2,16 +2,24 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\EmployeeResource\Pages;
-use App\Filament\Resources\EmployeeResource\RelationManagers;
-use App\Models\Employee;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
+use App\Models\City;
 use Filament\Tables;
+use App\Models\State;
+use App\Models\Country;
+use App\Models\Employee;
+use Filament\Forms\Form;
+use App\Models\Department;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\EmployeeResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\EmployeeResource\RelationManagers;
 
 class EmployeeResource extends Resource
 {
@@ -23,7 +31,30 @@ class EmployeeResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Section::make()->schema([
+                    TextInput::make('first_name')->required(),
+                    TextInput::make('last_name')->required(),
+                    TextInput::make('address')->required(),
+                    TextInput::make('zip_code')->required(),
+                    Select::make('country_id')
+                        ->label('Country')
+                        ->options(Country::all()->pluck('name', 'id'))
+                        ->searchable()->required(),
+                    Select::make('state_id')
+                        ->label('State')
+                        ->options(State::all()->pluck('name', 'id'))
+                        ->searchable()->required(),
+                    Select::make('city_id')
+                        ->label('City')
+                        ->options(City::all()->pluck('name', 'id'))
+                        ->searchable()->required(),
+                    Select::make('department_id')
+                        ->label('Department')
+                        ->options(Department::all()->pluck('name', 'id'))
+                        ->searchable()->required(),
+                    DatePicker::make('birth_date')->required(),
+                    DatePicker::make('date_hired')->required(),
+                ]),
             ]);
     }
 
